61f20311f3cf5999b33e611bd3205ea9edb4083f
albus2005 <aristotelu2005@gmail.com>
2026-05-19 22:28:32 +0200

    Pandas essai N°3 : Exploration

--- exercice_j19v1_pandas.py
+++ exercice_j19v1_pandas.py
@@ -9,7 +9,7 @@ data = {
 'poids': [12, 15, 10, 18, 14],
 'age': [8, 23, 45, 67, 34]
 }
-df = df = pd.DataFrame(data)
+df = pd.DataFrame(data)
 print(f"La feuille de donnees: {df}")
 print(df["age"])
 print(df[df['age'] >18])
