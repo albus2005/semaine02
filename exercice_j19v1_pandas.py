3b2cba775b999fa4705824dbba582552944b054a
albus2005 <aristotelu2005@gmail.com>
2026-05-19 22:27:15 +0200

    Pandas essai N°2 : Exploration

---
+++ exercice_j19v1_pandas.py
@@ -0,0 +1,17 @@
+import pandas as pd 
+
+'''
+feuille_des_donnees = pd.DataFrame(data)
+Ecrire(feuille de données )
+'''
+data = {
+'nom': ['Amani', 'Baraka', 'Furaha', 'Mapendo', 'Neema'],
+'poids': [12, 15, 10, 18, 14],
+'age': [8, 23, 45, 67, 34]
+}
+df = df = pd.DataFrame(data)
+print(f"La feuille de donnees: {df}")
+print(df["age"])
+print(df[df['age'] >18])
+
+
