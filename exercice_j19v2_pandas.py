782861402161cee6cb0d8b0cc5aa56c87bf774d1
albus2005 <aristotelu2005@gmail.com>
2026-05-19 23:01:31 +0200

    Pandas essai N°3 : Exploration

--- exercice_j19v2_pandas.py
+++ exercice_j19v2_pandas.py
@@ -1,1 +1,9 @@
-Chargement...
+import pandas as pd
+
+patients_registre = pd.read_csv("patients_bukavu.csv")
+patients_df = pd.DataFrame(patients_registre")
+print(patients_df)
+print(patients_df['age'])
+print(patients_df[['age'] > 12])
+
+
