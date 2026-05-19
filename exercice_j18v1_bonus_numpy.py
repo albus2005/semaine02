16d00b065649b8d3975a6e5c7d3766a40df252e3
albus2005 <aristotelu2005@gmail.com>
2026-05-19 22:14:49 +0200

    Deuxieume

---
+++ exercice_j18v1_bonus_numpy.py
@@ -0,0 +1,23 @@
+'''
+ages_tableau = np.array(ages) 
+age_std = np.std(ages_tableau)
+age_median = np.median(ages_tableau)
+age_moyenne = np.moyenne(ages_tableau)
+Écrire (age_median)
+Ecrire(age_moyenne)
+Ecrire(age_std)
+'''
+
+import numpy as np 
+ages = [8, 12, 45, 23, 67, 34, 5, 89, 43, 21]
+ages_tableau = np.array(ages) 
+age_std = np.std(ages_tableau)
+age_median = np.median(ages_tableau)
+age_moyenne = np.moyenne(ages_tableau)
+print(age_median)
+print(age_moyenne)
+print(age_std)
+
+
+
+
