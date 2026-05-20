5a007cdc2f2b81044764963bbb0d51b57a9ee34d
albus2005 <aristotelu2005@gmail.com>
2026-05-20 15:42:50 +0000

    Mes modifications locales

--- exercice_j19v2_pandas.py
+++ exercice_j19v2_pandas.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
 patients_registre = pd.read_csv("patients_bukavu.csv")
-patients_df = pd.DataFrame(patients_registre")
+patients_df = pd.DataFrame(patients_registre)
 print(patients_df)
-print(patients_df['age'])
-print(patients_df[['age'] > 12])
-
+print(patients_df['poids'])
+#poids = int(patients_df['poids'])
+print(patients_df[patients_df['poids'] > 12])
+print(patients_df.info())
+print(patients_df.describe())
 
