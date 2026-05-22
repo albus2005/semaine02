8f3d8f3f6771657b4ced2bf9fb2b995bf74cf86d
albus2005 <aristotelu2005@gmail.com>
2026-05-22 14:30:03 +0200

    Pandas

---
+++ exercice_j20v2_pandas.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+try:
+    patients_sale = pd.read_csv("hospital_sale.csv")
+except FileNotFoundError:
+    print("Fichier Introuvable. Verifier le chemin.")
+
+print("Contenu de patients_sale.csv")
+print(patients_sale.isnull())
+print(patients_sale.isnull().sum())
+
+patients_sans_doublon = patients_sale.drop_duplicates()
+
+patients_propres = patients_sans_doublon.copy()
+patients_propres['age'] = patients_propres['age'].fillna(patients_propres['age'].mean())
+patients_propres['poids'] = patients_propres['poids'].fillna(patients_propres['poids'].mean())
+
+patients_propres.loc[patients_propres['temperature'] > 42, 'temperature'] /= 10
+
+patients_propres['nom'] = patients_propres['nom'].str.capitalize()
+patients_propres['sexe'] = patients_propres['sexe'].str.capitalize()
+
+print("Data Frame propre :")
+print(patients_propres)
+print("Fin Du Programme")
