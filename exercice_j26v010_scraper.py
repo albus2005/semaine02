41cbdf5af588b47b0dab3d27be9ee0706352301a
albus2005 <aristotelu2005@gmail.com>
2026-06-02 21:48:01 +0200

    SCRAPER

--- exercice_j26v010_scraper.py
+++ exercice_j26v010_scraper.py
@@ -2,9 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-import requests
-import pandas as pd
-
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
 }
@@ -13,6 +10,9 @@ reponse = requests.get(
     'https://en.wikipedia.org/wiki/List_of_African_countries_by_population',
     headers=headers
 )
+
+
+soup = BeautifulSoup(reponse.text, 'html.parser')
 # Étape 3 : trouver le tableau
 tableau = soup.find('table')
 
