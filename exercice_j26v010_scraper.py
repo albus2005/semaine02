4e0a900db1a98099130209e5113cf04aa9e463c5
albus2005 <aristotelu2005@gmail.com>
2026-06-02 21:43:47 +0200

    SCRAPER

--- exercice_j26v010_scraper.py
+++ exercice_j26v010_scraper.py
@@ -2,14 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-reponse = requests.get('https://en.wikipedia.org/wiki/List_of_African_countries_by_population')
-soup = BeautifulSoup(reponse.text, 'html.parser')
+import requests
+import pandas as pd
 
-# Méthode simple et rapide
-df = pd.read_html(reponse.text)[0]
-print(df.head())
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+}
 
-# Ou méthode précise
+reponse = requests.get(
+    'https://en.wikipedia.org/wiki/List_of_African_countries_by_population',
+    headers=headers
+)
+# Étape 3 : trouver le tableau
 tableau = soup.find('table')
+
+# Étape 4 : lire avec Pandas
 df = pd.read_html(str(tableau))[0]
-print(df.head())
+print(df)
